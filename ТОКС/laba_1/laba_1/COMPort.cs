f266065ba663a09dcebb0c74ea3dfc1b5bcfb9e9
VladLinkevich <linkevichvv@mail.ru>
2020-11-19 13:33:18 +0300

    generate map

--- ТОКС/laba_1/laba_1/COMPort.cs
+++ ТОКС/laba_1/laba_1/COMPort.cs
@@ -10,6 +10,7 @@ namespace laba_1
 {
     class COMPort
     {
+        private const string STARTBIT = "01111110";
         private string _baudRate;
 
         private SerialPort _serialPort;
@@ -91,7 +92,7 @@ namespace laba_1
             try
             {
                 if (_serialPort.IsOpen != true) { _serialPort.Open(); }
-                _serialPort.Write(msg);
+                _serialPort.Write(Serialize(msg));
 
                 return true;
             } catch (Exception ex)
@@ -101,7 +102,66 @@ namespace laba_1
             }
         }
 
-        
+        private string Serialize(string msg)
+        {
+            string result = string.Empty;
+
+            foreach (char letter in msg){
+                for (int i = sizeof(char) * 8 - 1; i >= 0; i--)
+                {
+                    if ((letter & (1 << i)) != 0) { result += '1'; }
+                    else { result += '0'; }
+                }
+            }
+
+            int rep = 0;
+            for (int i = 0; i < result.Length; ++i)
+            {
+                if (result[i] == '1')
+                {
+                    rep++;
+                    if (rep == 5) {
+                        result = result.Insert(i+1, "0");
+                        ++i;
+                    }
+                }
+                else { rep = 0; }
+            }
+
+            result = result.Insert(0, STARTBIT);
+            result += STARTBIT;
+
+            return result;
+        }
+
+        private string Unserialize(string msg)
+        {
+            string b_msg = msg.Substring(8, msg.Length - 16);
+            string result = string.Empty;
+
+            int rep = 0;
+            for (int i = 0; i < b_msg.Length; ++i)
+            {
+                if (b_msg[i] == '1')
+                {
+                    rep++;
+                    if (rep == 5)
+                    {
+                        b_msg = b_msg.Substring(0, i+1) + b_msg.Substring(i+2, b_msg.Length - (i + 2));
+                    }
+                }
+                else { rep = 0; }
+            }
+
+            for (int i = 0, end = b_msg.Length / 16; i < end; ++i)
+            {
+                char buffer = (char)Convert.ToByte(b_msg.Substring(16 * i, 16), 2); ;
+                result += buffer;
+            }
+
+            return result;
+        }
+
         private void DisplayData(string msg)
         {
             _displayWindow.Invoke(new EventHandler(delegate
@@ -113,8 +173,8 @@ namespace laba_1
 
         private void comPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-                    string msg = _serialPort.ReadExisting();
-                    DisplayData(msg + "\r\n");               
+            string msg = Unserialize(_serialPort.ReadExisting());
+            DisplayData(msg + "\r\n");               
         }
     }
 }
